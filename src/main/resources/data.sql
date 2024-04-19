0039cb1343d2c123f590ab4bdaf279d04ea28229
JavaBrewer <hks0658@gmail.com>
2024-04-19 10:18:03 +0900

    add some features on yml and test

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE users (
-    userId BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255)
-);
--- src/test/java/com/example/demoh2/UserRepositoryTest.java
+++ src/test/java/com/example/demoh2/UserRepositoryTest.java
@@ -2,14 +2,17 @@ package com.example.demoh2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.yml")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
